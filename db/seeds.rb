9a0faa4085fca78f3195b7d74420e962e33a1dbd
Tim Fraczak <tfraczak@gmail.com>
2021-05-11 12:39:59 -0400

    adjusted some styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+drop_table :users
+
 User.create!(
     email: "teddy@gmail.com",
     username: "teddy",
