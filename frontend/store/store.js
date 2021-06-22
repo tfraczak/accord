f35ff02e68358d6e20f514a1699938be958e6e72
Tim Fraczak <tfraczak@gmail.com>
2021-06-22 09:52:04 -0400

    Add condition for removing logger in production branch

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,10 +4,18 @@ import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 import { loadState } from '../utils/state_utils';
 
+let middleware = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middleware = [...middleware, thunk, logger];
+} else {
+    middleware = [...middleware, thunk];
+}
+
 const configureStore = (preloadedState = {}) => {
     const persistedState = loadState();
     const newState = Object.assign({}, preloadedState, persistedState);
-    return createStore(rootReducer, newState, applyMiddleware(thunk, logger));
+    return createStore(rootReducer, newState, applyMiddleware(...middleware));
 }
 
 export default configureStore;
